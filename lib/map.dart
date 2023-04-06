f5f8464fce655536d486d7310caa659d7a6ddb85
oishigyunyu <yamamuuuura@gmail.com>
2023-04-06 23:08:55 +0900

    implementing map

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatefulWidget {
@@ -12,6 +14,67 @@ class _MapViewState extends State<MapView> {
   final CameraPosition _initialLocation =
       const CameraPosition(target: LatLng(0.0, 0.0));
   late GoogleMapController mapController;
+  late Position _currentPosition;
+  final TextEditingController startAddressController = TextEditingController();
+  final TextEditingController destinationAddressController =
+      TextEditingController();
+  final FocusNode startAddressFocusNode = FocusNode();
+  String _currentAddress = "";
+  String _startAddress = "";
+  String _destinationAddress = "";
+  String? _placeDistance;
+
+  _getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) async {
+      setState(() {
+        _currentPosition = position;
+        print('CURRENT POS: $_currentPosition');
+
+        mapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target:
+                  LatLng(_currentPosition.latitude, _currentPosition.longitude),
+              zoom: 18.0,
+            ),
+          ),
+        );
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddress() async {
+    try {
+      List<Placemark> p = await placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+      Placemark place = p[0];
+      setState(() {
+        _currentAddress =
+            "${place.name}, ${place.locality}, ${place.postalCode}, ${place.country}";
+        startAddressController.text = _currentAddress;
+        _startAddress = _currentAddress;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,9 +141,7 @@ class _MapViewState extends State<MapView> {
                   padding: const EdgeInsets.all(8.0),
                   child: FloatingActionButton.large(
                     onPressed: () {
-                      mapController.animateCamera(
-                        CameraUpdate.zoomIn(),
-                      );
+                      _getCurrentLocation();
                     },
                     child: Icon(
                       Icons.navigation,
