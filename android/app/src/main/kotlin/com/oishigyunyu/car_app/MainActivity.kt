c59b36eecf19256e7d87d336f56a6dd1edba63e8
oishigyunyu <yamamuuuura@gmail.com>
2023-04-10 00:08:18 +0900

    removed

--- android/app/src/main/kotlin/com/oishigyunyu/car_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/oishigyunyu/car_app/MainActivity.kt
@@ -9,26 +9,5 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 
 class MainActivity: FlutterActivity() {
-    private val CHANNEL = "appInfo"
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler{
-            call, result ->
-            if (call.method == "getAppVersion"){
-                try {
-                    val pInfo = applicationContext.packageManager.getPackageInfo(context.packageName, 0)
-                    val version = pInfo.versionName
-                    result.success("${pInfo.versionName}")
-                } catch (e: PackageManager.NameNotFoundException) {
-                    e.printStackTrace()
-                    result.notImplemented()
-                }
-            } else {
-                result.notImplemented()
-            }
-        }
-    }
-
 
 }
