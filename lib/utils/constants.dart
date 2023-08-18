e13a75888173cf2d3f62b6b0e131201b8610161d
oishigyunyu <yamamuuuura@gmail.com>
2023-08-19 01:52:38 +0900

    refactoring

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Map<String, IconData> iconsMaps = {
+  "Information": Icons.info_outline_rounded,
+  "Map": Icons.place_outlined,
+  "Maintenance": Icons.car_repair_outlined,
+  "Music": Icons.music_note_outlined,
+  "Settings": Icons.settings,
+  "FireStore": Icons.local_fire_department_outlined,
+};
